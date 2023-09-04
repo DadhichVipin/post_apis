1d63624f625c835fe38d9d65e6037cf8a02e51d4
Vipin <107750@dypatiluniversitypune.edu.in>
2023-09-04 19:13:20 +0530

    Developed API to perform CURD operation

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Post
+from .serializers import PostSerializer
+from rest_framework import permissions
 
-# Create your views here.
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+
+    def perform_create(self, serializer):
+        # Set the author of the post to the current authenticated user
+        serializer.save(author=self.request.user)
